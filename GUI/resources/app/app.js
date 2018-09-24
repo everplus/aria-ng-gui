1843329003cd08a5e892918eaa332e058cc82d93
Xmader <xmader@outlook.com>
2018-09-24 19:02:00 +0800

    优化

--- GUI/resources/app/app.js
+++ GUI/resources/app/app.js
@@ -2,21 +2,13 @@ const os = require('os');
 const electron = require('electron');
 const path = require('path');
 
-
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const Menu = electron.Menu;
-const Tray = electron.Tray;
+const { app, BrowserWindow } = electron
 
 var mainWindow = null;
-var tray = null;
-var platform=os.platform();
-if (platform == 'linux'){
-    var aria2_bin = "aria2c"
-}
-else{
-    var aria2_bin = "aria2c.exe"
-}
+var platform = os.platform();
+
+var aria2_bin = (platform == 'linux' || platform == 'darwin') ? "aria2c" : "aria2c.exe"
+
 
 app.on('window-all-closed', function () {
     app.quit();
@@ -35,19 +27,17 @@ app.on('ready', function () {
             nodeIntegration: false
         }
     });
-    
+
     var aria2_dir = __dirname + '/aria2/' + aria2_bin
-    
+
     //打开主程序
-    var subpy = require('child_process').spawn(aria2_dir, ["--conf-path="+__dirname + '/aria2/aria2.conf']);
-    //console.log(subpy)
-    
+    var subpy = require('child_process').spawn(aria2_dir, [`--conf-path=${__dirname}/aria2/aria2.conf`]);
+
     // 打开窗口的调试工具
     //mainWindow.webContents.openDevTools();
 
     mainWindow.setMenu(null);
-    mainWindow.loadURL('file://' + __dirname + '/pages/index.html');
-    //mainWindow.loadURL('https://xmader.coding.me/aria-ng/');
+    mainWindow.loadURL(`file://${__dirname}/pages/index.html`);
 
     mainWindow.once('ready-to-show', function () {
         mainWindow.show()
